5fedfb6577a2c1e6b2c887843a86c1e607b2b8d8
Somi <tati.contreras707@gmail.com>
2026-01-16 00:28:46 -0600

    Greater than average feature
    hopefully this will allow you to comment on all of my code since i originally messed it up.

--- main.cpp
+++ main.cpp
@@ -26,3 +26,33 @@ vector<int> getNumbers()
     }
     return nums;
 }
+//--------------Greater than average
+int getAverage(vector<int> nums)
+{
+    int avgNum = 0; // avgNums stores the numbers that are added together
+    for (int i = 0; i < nums.size(); i++)
+    {
+        avgNum += nums[i]; // go through the list
+        // add the number to avgNum
+    }
+    avgNum = avgNum / nums.size(); // Once all the numbers are added together, we divide by the size (how to get the average)
+    return avgNum;                 // return the average
+}
+
+int numberGreaterThanAverage(vector<int> nums)
+{
+    // TODO Student 2
+    // Findout how many numbers are greater than the average of all of the numbers
+    int avgNum = getAverage(nums); // we send the list to getAverage, which will return the average back to us
+    int total = 0;                 // this will add how many numbers are greater than the average
+    for (int num : nums)
+    {
+        // go through the list again
+        if (num > avgNum)
+        {
+            // if the number we are on is greater than the average
+            total += 1; // add 1 to toal
+        }
+    }
+    return total; // we reurn how many nymbers were greater than the average;
+}
